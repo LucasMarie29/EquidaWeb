0c30ebebc878b8db6049ad2e43dfc9bfd07a4bc1
sio2 <marielucas665@gmail.com>
2025-09-24 09:43:18 +0200

    Ajout DaoVente

--- src/main/java/test/TestConnexionBdd.java
+++ src/main/java/test/TestConnexionBdd.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 
 import database.Connexionbdd;
 import database.DaoCheval;
+import database.DaoVente;
+import model.Lieu;
+import model.Vente;
 
 public class TestConnexionBdd {
 
@@ -12,8 +15,20 @@ public class TestConnexionBdd {
 
         Connection cnx = Connexionbdd.ouvrirConnexion();
         System.out.println ("nombre de chevaux = " + DaoCheval.getLesChevaux(cnx).size());
+        System.out.println("Le cheval = " + DaoCheval.getLeCheval(cnx, 2).getNom());
 
-
+        System.out.println ("nombre de ventes = " + DaoVente.getLesVentes(cnx).size());
+        System.out.println("La vente = " + DaoVente.getLaVente(cnx, 1).getNom());
+        
+        
+        Vente vente = new Vente();
+        vente.setNom("test3");
+        vente.setDateDebutVente("2025-10-12");
+        
+        
+        
+        DaoVente.ajouterVente(cnx, vente);
+        
             }
 
 }
